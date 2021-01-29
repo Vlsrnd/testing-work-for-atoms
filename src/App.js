c4c62287bb5f69a112b13eb5ea7bc93eab10e7ec
Leonid Velichko <vlsrnd@gmail.com>
2021-01-29 20:39:01 +0300

    feat(App.js, package.json): add BrowserRouter, add nav structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
 import './App.css';
 
 const Header = (props) => {
-  return <div>Header</div>
+  const navLinks = [
+    {link: '', text: 'главная'},
+    {link: 'rules', text:'правила акции'}, 
+    {link: 'prizes', text: 'призы'}, 
+    {link: 'winners', text: 'победители'}, 
+    {link: 'feedback', text: 'обратная связь'}, 
+    {link: 'login', text: 'войти'}]
+    .map(title => <li><NavLink to={title.link}>{title.text}</NavLink></li>);
+  return (
+    <ul>
+      {navLinks};
+    </ul>
+  )
 };
 const Main = (props) => {
   return <div>Main</div>
 }
+const Rules = () => <div>there is rules</div>;
+const Prizes = () => <div>there is prizes</div>;
+const Winners = () => <div>there is winners</div>;
+const Feedback = () => <div>there is feedback</div>;
+const Login = () => <div>there is login</div>;
 
 const App = () => {
   return (
+
     <div className='container'>
       <Header />
-      <Main />
+      <main>
+        <Route path='' render={() => <Main />} />
+        <Route path='/rules' render={() => <Rules />} />
+        <Route path='/prizes' render={() => <Prizes />} />
+        <Route path='/winners' render={() => <Winners />} />
+        <Route path='/feedback' render={() => <Feedback />} />
+        <Route path='/login' render={() => <Login />} />
+      </main>
     </div>
   );
 }
 
-export default App;
+const WrappedApp = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+}
+
+export default WrappedApp;
