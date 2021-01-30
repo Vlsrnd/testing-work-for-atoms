d11a88cad870b44830167fd7d7e87dae296c1cec
Leonid Velichko <vlsrnd@gmail.com>
2021-01-30 18:26:48 +0300

    feat(Main, Main.module.css): add  3 concentric divs

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,27 @@
 import ConcentricDiv from './ConcentricDiv/ConcentricDiv';
 import styles from './Main.module.css';
 
+import prizesImg from '../../assets/img/prizes.png';
+import superPrizeImg from '../../assets/img/super-prize.png';
+
 const Main = (props) => {
-  const text = <div>tesdfsxt</div>;
+  const text = <div>ЗАРЕГИСТРИРОВАТЬ ЧЕК</div>;
+  const prizesImgElement = <img src={prizesImg} alt='prizes' />;
+  const superPrizeImgElement = <img src={superPrizeImg} alt='super prize' />;
   return (
     <div>
       <div className={styles.greenCircle}></div>
-      <ConcentricDiv element={text} width={150} height={150}/>
+      <div className={styles.leftConcentricDiv}>
+        <ConcentricDiv element={text} width={284} height={60} numberLines={3} />
+      </div>
+      <div className={styles.rightTopConcentricDiv}>
+        <ConcentricDiv element={prizesImgElement}
+          width={126} height={126} />
+      </div>
+      <div className={styles.rightBottomConcentricDiv}>
+        <ConcentricDiv element={superPrizeImgElement}
+          width={132} height={129} />
+      </div>
     </div>
   )
 };
