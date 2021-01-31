5e3af9c807a1077dd901c33e7e6bb424d7311f84
Leonid Velichko <vlsrnd@gmail.com>
2021-01-31 22:26:26 +0300

    feat(Login): add Login component and style for it

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Login from './components/Login/Login';
 import Main from './components/Main/Main';
 
 const Rules = () => <div className='emptyPage' >rules</div>;
 const Prizes = () => <div className='emptyPage' >prizes</div>;
 const Winners = () => <div className='emptyPage' >winners</div>;
 const Feedback = () => <div className='emptyPage' >feedback</div>;
-const Login = () => <div>there is login</div>;
 
 const App = () => {
   return (
     <div className='app'>
       <Header />
+      <Login />
       <main className='container'>
         <Route exact path='/' render={() => <Main />} />
         <Route path='/rules' render={() => <Rules />} />
