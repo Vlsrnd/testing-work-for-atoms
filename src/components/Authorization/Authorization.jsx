84110a37fb6adc5bb1ffce1022b0ecb31b5c6301
Leonid Velichko <vlsrnd@gmail.com>
2021-02-01 20:57:14 +0300

    feat(Authorization): pass hideAuthorization to Login and Registration Components

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -3,14 +3,14 @@ import { Login } from './Login/Login';
 import { Registration } from './Registration/Registration';
 import styles from './Authorization.module.css';
 
-const Authorization = () => {
+const Authorization = ({hideAuthorization}) => {
   const [isLoginMode, setMode] = useState(true);
   const setLoginMode = () => setMode(true);
   const setRegistrationMode = () => setMode(false);
   return (
     <div className={styles.modal}>
-      {isLoginMode && <Login setRegistrationMode={setRegistrationMode}/>}
-      {!isLoginMode && <Registration setLoginMode={setLoginMode}/>}
+      {isLoginMode && <Login setRegistrationMode={setRegistrationMode} hideAuthorization={hideAuthorization} />}
+      {!isLoginMode && <Registration setLoginMode={setLoginMode} hideAuthorization={hideAuthorization} />}
     </div>
   )
 };
