5fbafcf3791b6a1c800f759824880be64eff44a4
Leonid Velichko <vlsrnd@gmail.com>
2021-02-01 20:56:17 +0300

    feat(App): add state for show/hide login component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Authorization from './components/Authorization/Authorization';
 import Main from './components/Main/Main';
+import { useState } from 'react';
 
 const Rules = () => <div className='emptyPage' >rules</div>;
 const Prizes = () => <div className='emptyPage' >prizes</div>;
@@ -10,10 +11,13 @@ const Winners = () => <div className='emptyPage' >winners</div>;
 const Feedback = () => <div className='emptyPage' >feedback</div>;
 
 const App = () => {
+  const [isAuthorizationVisible, setAuthorizationVisibility] = useState(false);
+  const showAuthorization = () => setAuthorizationVisibility(true);
+  const hideAuthorization = () => setAuthorizationVisibility(false);
   return (
     <div className='app'>
-      <Header />
-      <Authorization />
+      <Header showAuthorization={showAuthorization} />
+      {isAuthorizationVisible && <Authorization hideAuthorization={hideAuthorization}/>}
       <main className='container'>
         <Route exact path='/' render={() => <Main />} />
         <Route path='/rules' render={() => <Rules />} />
