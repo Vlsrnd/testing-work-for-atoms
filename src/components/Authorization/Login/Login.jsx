92ea131c6a7c56959a7e9d3fc46c417b3b8cb855
Leonid Velichko <vlsrnd@gmail.com>
2021-02-01 20:55:09 +0300

    feat(Login, Login.module.css): add close btn, style and functional for it

--- src/components/Authorization/Login/Login.jsx
+++ src/components/Authorization/Login/Login.jsx
@@ -1,23 +1,29 @@
 import { useForm } from 'react-hook-form';
 import styles from './Login.module.css';
 
-export const Login = ({setRegistrationMode}) => {
-  const {register, handleSubmit, errors} = useForm();
+import closeBtn from '../../../assets/img/mobile-menu-btn-close.png';
+
+export const Login = ({setRegistrationMode, hideAuthorization}) => {
+  const {register, handleSubmit, errors} = useForm({
+    mode: 'onTouched',
+    shouldFocusError: false
+  });
   const onSubmit = (data) => console.log(data);
   return (
     <div className={styles.login}>
       <form onSubmit={handleSubmit(onSubmit)}>
-          <header>Авторизация</header>
-          <div className={styles.email}>
-            <input ref={register({required: true})} name='first_name' 
-              className={errors.first_name && styles.error} type='text' placeholder='Имя' />
-          </div>
-          <div className={styles.password}>
-            <input ref={register({required: true})} name='last_name' 
-              className={errors.last_name && styles.error} type='text' placeholder='Фамилия' />
-          </div>
-          <div className={styles.forgetPassword}>Забыл пароль?</div>
-          <button className={styles.submitBtn} type='submit'>ВОЙТИ</button>
+        <img className={styles.closeBtn} src={closeBtn} alt='X' onClick={hideAuthorization} />
+        <header>Авторизация</header>
+        <div className={styles.email}>
+          <input ref={register({required: true})} name='username' 
+            className={errors.username && styles.error} type='email' placeholder='E-mail' />
+        </div>
+        <div className={styles.password}>
+          <input ref={register({required: true, minLength: 7})} name='password' 
+            className={errors.password && styles.error} type='password' placeholder='Пароль' />
+        </div>
+        <div className={styles.forgetPassword}>Забыл пароль?</div>
+        <button className={styles.submitBtn} type='submit'>ВОЙТИ</button>
       </form>
       <button onClick={setRegistrationMode}>Регистрация</button>
     </div>
