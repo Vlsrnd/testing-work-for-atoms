1449fa8a18c9fd6e70552217b537b50a3ee77dbd
Leonid Velichko <vlsrnd@gmail.com>
2021-02-02 22:40:26 +0300

    feat(Login): add local state for visible notification, notification text, buttons blocking. Add block and unblock function. edit onSubmit. add Notification. Add disabled attribute for buttons.

--- src/components/Authorization/Login/Login.jsx
+++ src/components/Authorization/Login/Login.jsx
@@ -2,15 +2,41 @@ import { useForm } from 'react-hook-form';
 import styles from './Login.module.css';
 
 import closeBtn from '../../../assets/img/mobile-menu-btn-close.svg';
+import { requestToAuthorize } from '../../../api/api';
+import { useState } from 'react';
+import { Notification } from '../Notification/Notification';
 
-export const Login = ({setRegistrationMode, hideAuthorization}) => {
+export const Login = ({setRegistrationMode, hideAuthorization, setAuth, setState}) => {
+  const [isNotificationVisible, setVisibleNotification] = useState(false);
+  const [notificationText, setNotificationText] = useState('');
+  const [isBlocked, setBlock] = useState(false);
+  const block = () => setBlock(true);
+  const unblock = () => setBlock(false);
+
   const {register, handleSubmit, errors} = useForm({
     mode: 'onTouched',
     shouldFocusError: false
   });
-  const onSubmit = (formData) => console.log(formData);
+
+  const onSubmit = async (formData) => {
+    block()
+    try {
+      const response = await requestToAuthorize(formData);
+      const {message, data} = response;
+      setNotificationText(message);
+      setVisibleNotification(true);
+      setAuth(true);
+      setState(data);
+    } catch (err) {
+      alert(err);
+      unblock();
+    }
+  }
+
   return (
     <div className={styles.login}>
+      {isNotificationVisible && <Notification text={notificationText} unblock={unblock}
+        setVisibleNotification={setVisibleNotification} hideAuthorization={hideAuthorization} />}
       <form onSubmit={handleSubmit(onSubmit)}>
         <img className={styles.closeBtn} src={closeBtn} alt='X' onClick={hideAuthorization} />
         <header>Авторизация</header>
@@ -19,13 +45,13 @@ export const Login = ({setRegistrationMode, hideAuthorization}) => {
             className={errors.username && styles.error} type='email' placeholder='E-mail' />
         </div>
         <div className={styles.password}>
-          <input ref={register({required: true, minLength: 7})} name='password' 
+          <input ref={register({required: true, minLength: 6})} name='password' 
             className={errors.password && styles.error} type='password' placeholder='Пароль' />
         </div>
         <div className={styles.forgetPassword}>Забыл пароль?</div>
-        <button className={styles.submitBtn} type='submit'>ВОЙТИ</button>
+        <button disabled={isBlocked} className={styles.submitBtn} type='submit'>ВОЙТИ</button>
       </form>
-      <button onClick={setRegistrationMode}>Регистрация</button>
+      <button disabled={isBlocked} className={styles.registrationBtn} onClick={setRegistrationMode}>Регистрация</button>
     </div>
   )
 };
