04b930feef664aaf59b675b0bb981cce1b26a05a
Leonid Velichko <vlsrnd@gmail.com>
2021-02-02 22:34:11 +0300

    feat(Authorization): pass setAuth and setState to Login component

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -3,14 +3,16 @@ import { Login } from './Login/Login';
 import { Registration } from './Registration/Registration';
 import styles from './Authorization.module.css';
 
-const Authorization = ({hideAuthorization}) => {
+const Authorization = ({hideAuthorization, setAuth, setState}) => {
   const [isLoginMode, setMode] = useState(true);
   const setLoginMode = () => setMode(true);
   const setRegistrationMode = () => setMode(false);
   return (
     <div className={styles.modal}>
-      {isLoginMode && <Login setRegistrationMode={setRegistrationMode} hideAuthorization={hideAuthorization} />}
-      {!isLoginMode && <Registration setLoginMode={setLoginMode} hideAuthorization={hideAuthorization} />}
+      {isLoginMode && <Login setRegistrationMode={setRegistrationMode} 
+        hideAuthorization={hideAuthorization} setAuth={setAuth} setState={setState} />}
+      {!isLoginMode && <Registration setLoginMode={setLoginMode} 
+        hideAuthorization={hideAuthorization} />}
     </div>
   )
 };
