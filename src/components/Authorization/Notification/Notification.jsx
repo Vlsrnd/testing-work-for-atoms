4540af1f6c288733bb71d8e27bff97bd07814558
Leonid Velichko <vlsrnd@gmail.com>
2021-02-02 21:38:31 +0300

    feat(Notification): add unlock button after click ok

--- src/components/Authorization/Notification/Notification.jsx
+++ src/components/Authorization/Notification/Notification.jsx
@@ -1,14 +1,15 @@
 import styles from './Notification.module.css';
 
-export const Notification = ({text, setVisibleNotification, setLoginMode}) => {
-  const closeAndSwitchToLogin = () => {
+export const Notification = ({text, unblock, setVisibleNotification, setLoginMode}) => {
+  const closeUnblockAndSwitchToLogin = () => {
+    unblock();
     setVisibleNotification(false);
     setLoginMode();
   }
   return (
     <div className={styles.notification}>
       <div className={styles.text}>{text}</div>
-      <button className={styles.ok} onClick={closeAndSwitchToLogin}>Ok</button>
+      <button className={styles.ok} onClick={closeUnblockAndSwitchToLogin}>Ok</button>
     </div>
   )
 };
